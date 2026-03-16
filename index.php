c537c0cb9bdc23c5c831ffc4e645a8b3960a935e
westleyxjames <westleyjames7899@gmail.com>
2026-03-16 20:21:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-       <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
-<!-- Histats.com  START  (aync)-->
+   <!-- Histats.com  START  (aync)-->
 <script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,5011280,4,511,95,18,00000000']);
+_Hasync.push(['Histats.start', '1,5014835,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
@@ -14,7 +13,7 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5011280&101" alt="web counter" border="0"></a></noscript>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5014835&101" alt="hit counter" border="0"></a></noscript>
 <!-- Histats.com  END  -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
